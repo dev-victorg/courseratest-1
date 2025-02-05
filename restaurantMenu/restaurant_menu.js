5ad7a5df44d41be54de05cb02d12a57acec6fe52
Victor Gonzalez <victor.gonzales@staff.flag.org>
2025-02-05 18:49:13 -0500

    Added the finishing touch to the drill

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -4,3 +4,15 @@ const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad']
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
     document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML; 
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+    mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
+});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
+}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
